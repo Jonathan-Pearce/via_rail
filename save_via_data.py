ff4bee7cf727bd7a328ae284938b60a66b4dc51b
Jonathan Pearce <Jonathan-Pearce@users.noreply.github.com>
2025-03-15 15:58:16 -0400

    Update save_via_data.py

--- save_via_data.py
+++ save_via_data.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime
 
-date = datetime.now().strftime('%Y%m%d')
+date = datetime.now()#.strftime('%Y%m%d')
 myFileName = f'raw_data/Via_data_{date}.json'
 
 data_json = requests.get('https://tsimobile.viarail.ca/data/allData.json') 
